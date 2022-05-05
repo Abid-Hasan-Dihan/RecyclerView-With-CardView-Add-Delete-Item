a4ee4ab4da3e85e570d3de354ff1b9b754c44e16
Abid-Hasan-Dihan <ahdihan2001aug@gmail.com>
2022-05-05 08:20:08 -0700

    initial commit

---
+++ app/src/main/java/com/example/recyclerviewandcardvviewproject/ExampleAdapter.java
@@ -0,0 +1,94 @@
+package com.example.recyclerviewandcardvviewproject;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+
+public class ExampleAdapter extends RecyclerView.Adapter<ExampleAdapter.ExampleViewHolder> {
+    private ArrayList<ExampleItem> mExampleList;
+    private OnItemClickListener mListener;
+
+    public interface OnItemClickListener{
+        void onItemClick(int position);
+        void onDeleteClick(int position);
+    }
+
+   public void setOnItemClickListener(OnItemClickListener listener){
+        mListener = listener;
+   }
+
+    public static class ExampleViewHolder extends RecyclerView.ViewHolder{
+        public ImageView mImageView;
+        public TextView mTextView1;
+        public TextView mTextView2;
+        public ImageView mDeleteImage;
+
+        public ExampleViewHolder(@NonNull View itemView, OnItemClickListener listener) {
+            super(itemView);
+            mImageView = itemView.findViewById(R.id.imageView);
+            mTextView1 = itemView.findViewById(R.id.textView);
+            mTextView2 = itemView.findViewById(R.id.textView2);
+            mDeleteImage = itemView.findViewById(R.id.image_delete);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (listener != null){
+                        int position = getAdapterPosition();
+                        if (position != RecyclerView.NO_POSITION){
+                            listener.onItemClick(position);
+                        }
+                    }
+                }
+            });
+
+            mDeleteImage.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (listener != null){
+                        int position = getAdapterPosition();
+                        if (position != RecyclerView.NO_POSITION){
+                            listener.onDeleteClick(position);
+                        }
+                    }
+                }
+            });
+        }
+    }
+
+    public ExampleAdapter(ArrayList<ExampleItem> exampleList) {
+        mExampleList = exampleList;
+    }
+
+    @NonNull
+    @Override
+    public ExampleViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.example_item, viewGroup, false);
+        ExampleViewHolder evh = new ExampleViewHolder(v, mListener);
+        return evh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ExampleViewHolder exampleViewHolder, int i) {
+        ExampleItem currentItem = mExampleList.get(i);
+
+        exampleViewHolder.mImageView.setImageResource(currentItem.getmImageResource());
+        exampleViewHolder.mTextView1.setText(currentItem.getmText1());
+        exampleViewHolder.mTextView2.setText(currentItem.getmText2());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mExampleList.size();
+    }
+}
